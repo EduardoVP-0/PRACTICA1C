a7c82fd92bb4d8f18871c66b949a09f8830713f9
EduardoVP-0 <144479949+EduardoVP-0@users.noreply.github.com>
2023-09-24 15:20:13 -0600

    Agregar archivos de proyecto.

---
+++ PRACTICA1.cpp
@@ -0,0 +1,25 @@
+//MANEJO DE MEMORIA DINAMICA PRACTICA 1 C++
+//EDUARDO VELAZQUEZ 
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main() {
+	int numeros[6] = { 10,20,30,40,50,60 }, n[] = { 3,4,5 };
+	char c[] = { 'L','u','i','s' }, s[] = "Mona Lisa";
+
+	cout << numeros[1] << endl;
+	cout << n[2] << endl;
+	cout << c[0] << endl;
+	cout << c[1] << endl;
+	cout << s << endl;
+
+
+
+
+
+
+	system("PAUSE");
+	return 0;
+}
