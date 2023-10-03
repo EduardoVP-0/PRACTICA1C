23ae6f9e97428331d15374e62f249359f60995c7
EduardoVP-0 <144479949+EduardoVP-0@users.noreply.github.com>
2023-10-02 19:26:11 -0600

    ARREGLO

--- PRACTICA1.cpp
+++ PRACTICA1.cpp
@@ -7,18 +7,13 @@
 using namespace std;
 int main() {
 	int numeros[6] = { 10,20,30,40,50,60 }, n[] = { 3,4,5 };
-	char c[] = { 'L','u','i','s' }, s[] = "Mona Lisa";
+	char c[] = { 'L','a','l','o' }, s[] = "Mona Lisa";
 
 	cout << numeros[1] << endl;
 	cout << n[2] << endl;
 	cout << c[0] << endl;
 	cout << c[1] << endl;
 	cout << s << endl;
-
-
-
-
-
 
 	system("PAUSE");
 	return 0;
